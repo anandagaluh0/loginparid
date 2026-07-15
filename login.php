b48fd921bd46d149b6bc0196415d0c29170da642
anandagaluh0 <anandagaluh0@gmail.com>
2026-07-15 14:50:30 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,25 @@
 <?php
 include "koneksi.php";
 
-if(isset($_GET['username']) && isset($_GET['password'])){
-    $username = $_GET['username'];
-    $password = $_GET['password'];
-    
-    $query = $koneksi->prepare("SELECT * FROM user WHERE username = :user AND password = :pass");
-    $query->execute(['user' => $username, 'pass'=> $password]);
-    $data = $query->fetch();
-    
+// UBAH DARI $_GET MENJADI $_POST
+if(isset($_POST['username']) && isset($_POST['password'])){
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // cek username dan password
+    $sql = "SELECT * FROM users 
+            WHERE username = :username 
+            AND password = :password";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([
+        ':username' => $username,
+        ':password' => $password
+    ]);
+
+    $data = $stmt->fetch();
+
     if($data){
         echo "berhasil";
     }else{
